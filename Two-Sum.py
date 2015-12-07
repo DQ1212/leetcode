4920137cc579fb6b40c4f95d9c864dc0126ca683
DQ1212 <540388119@qq.com>
2015-12-07 14:50:36 +0800

    Create Two-Sum.py

---
+++ Two-Sum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        hash = {}
+        for i in xrange(0, len(nums)):
+            if target-nums[i] in hash:
+                return [hash[target-nums[i]]+1, i+1]
+                
+            hash[nums[i]] = i
